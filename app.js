debf83a4ab3ea396031520b937ae84db40992a1d
ale-x-sis <alexsis.bass@gmail.com>
2026-05-26 18:34:40 -0400

    Update app.js

--- app.js
+++ app.js
@@ -728,7 +728,7 @@ function handleEpisodeJump(event) {
     const panel = target.closest("details");
     if (panel) panel.open = true;
     target.scrollIntoView({ behavior: "smooth", block: "center" });
-location.hash = link.dataset.episodeJump;
+history.replaceState(null, "", `#${link.dataset.episodeJump}`);
   });
 }
 
